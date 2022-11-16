62a80f3bc06edc07c12d1983f19675439d92d6fd
danifaso02 <danifaso02@gmail.com>
2022-11-16 16:14:37 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+const { createApp } = Vue
+
+const app = createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+}).mount('#app')
